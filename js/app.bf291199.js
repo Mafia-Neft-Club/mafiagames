8d0b08e9fa1e535586a8c64d06af6842143012da
Mafia-Neft-Club <94564571+Mafia-Neft-Club@users.noreply.github.com>
2025-10-19 22:29:43 +0300

    Update app.bf291199.js

--- js/app.bf291199.js
+++ js/app.bf291199.js
@@ -867,19 +867,6 @@
                                     { value: "sherif", text: "Шер" },
                                 ],
                                 selectPointOptions: [
-                                    { value: "-0.1", text: "-0.1" },
-{ value: "-0.2", text: "-0.2" },
-{ value: "-0.3", text: "-0.3" },
-{ value: "-0.4", text: "-0.4" },
-{ value: "-0.5", text: "-0.5" },
-{ value: "-0.6", text: "-0.6" },
-{ value: "-0.7", text: "-0.7" },
-{ value: "-0.8", text: "-0.8" },
-{ value: "-0.9", text: "-0.9" },
-{ value: "-1", text: "-1" },
-{ value: "-1.1", text: "-1.1" },
-{ value: "-1.2", text: "-1.2" },                                    
-                                    { value: "0", text: "0" },
                                     { value: "0.1", text: "0.1" },
 { value: "0.2", text: "0.2" },
 { value: "0.3", text: "0.3" },
@@ -891,7 +878,20 @@
 { value: "0.9", text: "0.9" },
 { value: "1", text: "1" },
 { value: "1.1", text: "1.1" },
-{ value: "1.2", text: "1.2" },
+{ value: "1.2", text: "1.2" },                                    
+                                    { value: "0", text: "0" },
+                                    { value: "-0.1", text: "-0.1" },
+{ value: "-0.2", text: "-0.2" },
+{ value: "-0.3", text: "-0.3" },
+{ value: "-0.4", text: "-0.4" },
+{ value: "-0.5", text: "-0.5" },
+{ value: "-0.6", text: "-0.6" },
+{ value: "-0.7", text: "-0.7" },
+{ value: "-0.8", text: "-0.8" },
+{ value: "-0.9", text: "-0.9" },
+{ value: "-1", text: "-1" },
+{ value: "-1.1", text: "-1.1" },
+{ value: "-1.2", text: "-1.2" },
                                     
                                 ],
                             };
@@ -1158,4 +1158,5 @@
     },
 });
 //# sourceMappingURL=app.bf291199.js.map
+
 
